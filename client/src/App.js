77e309ebd63080bf156659d37f84394ebb88a768
Phillip Merriman <phillipmerriman@gmail.com>
2021-03-12 11:27:24 -0600

    Login page template

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,12 @@ import ActivityInfo from "../src/pages/activity-info/index";
 import CreateActivity from "../src/pages/create-activity/index";
 import CreateOrSelect from "./pages/create-select-activities";
 import DaySchedule from "./pages/day-schedule/index";
+import LoginPage from "./pages/login-create-acct/index";
 
 function App() {
   return (
     <BrowserRouter>
-      <DaySchedule />
+      <LoginPage />
       {/* <Navbar />
       <h1 className="text-green-600 font-bold pacifico-title">YoGo</h1> */}
     </BrowserRouter>
---
+++ client/src/pages/login-create-acct/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+export default function LoginPage () {
+    return (
+        <>
+            <Navbar />
+      <div className="w-full max-w-sm quicksand-body">
+          <div className="relative flex items-center justify-center h-16">
+              <h1 className="pacifico-title">
+                  Take Care Of Yourself
+              </h1>
+          </div>
+        <div className="relative flex items-center justify-center h-16">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            LOGIN
+          </button>
+        </div>
+        <div className="relative flex items-center justify-center h-16">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            CREATE ACCOUNT
+          </button>
+        </div>
+      </div>
+        </>
+    );
+};
