b730d1d86d4295ff10cd6eee5c44e645d642b5c8
phillipmerriman <phillipmerriman@gmail.com>
2021-03-11 21:20:53 -0600

    Activity info page template setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
   
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
+import ActivityInfo from "../src/pages/activity-info/index";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <h1 className="text-green-600 font-bold pacifico-title">YoGo</h1>
+      <ActivityInfo />
+      {/* <Navbar />
+      <h1 className="text-green-600 font-bold pacifico-title">YoGo</h1> */}
     </BrowserRouter>
   );
 }
