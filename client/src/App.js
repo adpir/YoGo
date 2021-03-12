33191caf4d9583a13f23f5605a5d126e5df1ea1e
phillipmerriman <phillipmerriman@gmail.com>
2021-03-11 21:43:12 -0600

    Create Activity page template setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
 import ActivityInfo from "../src/pages/activity-info/index";
+import CreateActivity from "../src/pages/create-activity/index";
 
 function App() {
   return (
     <BrowserRouter>
-      <ActivityInfo />
+      <CreateActivity />
       {/* <Navbar />
       <h1 className="text-green-600 font-bold pacifico-title">YoGo</h1> */}
     </BrowserRouter>
