cfc3838f09a82ab9eae43d959fbb647b725c388e
Phillip Merriman <phillipmerriman@gmail.com>
2021-03-12 11:07:34 -0600

    CreateOrSelect page template

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
 import ActivityInfo from "../src/pages/activity-info/index";
 import CreateActivity from "../src/pages/create-activity/index";
+import CreateOrSelect from "./pages/create-select-activities";
 
 function App() {
   return (
     <BrowserRouter>
-      <CreateActivity />
+      <CreateOrSelect />
       {/* <Navbar />
       <h1 className="text-green-600 font-bold pacifico-title">YoGo</h1> */}
     </BrowserRouter>
--- client/src/pages/activity-info/index.js
+++ client/src/pages/activity-info/index.js
@@ -31,4 +31,4 @@ export default function ActivityInfo() {
       </div>
     </>
   );
-}
+};
