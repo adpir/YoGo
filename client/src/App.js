219a151ac40e95d208fefed0a3092401b5a767de
Alice Piar <adpir30@gmail.com>
2021-03-13 10:43:56 -0600

    button components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React from "react";  
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
+import CircleButton from "./components/CircleButton";
+import RectangleBtn from "./components/RectangleBtn";
 import ActivityInfo from "../src/pages/activity-info/index";
 import CreateActivity from "../src/pages/create-activity/index";
 import CreateOrSelect from "./pages/create-select-activities";
 import DaySchedule from "./pages/day-schedule/index";
 import LoginPage from "./pages/login-create-acct/index";
 import SelectActivity from "./pages/select-activity";
+
 
 function App() {
   return (
---
+++ client/src/components/CircleButton/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unknown-property */
+import React from "react";
+
+function CircleButton() {
+    return (
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Button
+        </button>
+    )
+}
+
+
+export default CircleButton;
