15fe5047df09fe98c6e9314aafcc43f54cdc8faa
Alice Piar <adpir30@gmail.com>
2021-03-14 15:27:59 -0500

    adding Link to the day-schedule page

--- client/src/pages/day-schedule/index.js
+++ client/src/pages/day-schedule/index.js
@@ -6,40 +6,43 @@ export default function DaySchedule() {
   return (
     <>
       <Navbar />
-      <div className="w-full max-w-sm quicksand-body">
+      <section className="w-full max-w-sm quicksand-body">
         <div className="relative flex items-center justify-center h-16">
-          <button className="bg-white w-screen mx-16 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          <div className="bg-white text-center w-screen mx-16 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
             TODAY IS THE DAY!
-          </button>
+          </div>
         </div>
-        <div className="relative flex items-center justify-center h-16">
-          <button className="pacifico-title bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            M
+        <Link to="/activity-info" >
+          <div className="relative flex items-center justify-center h-16">
+            <button className="pacifico-title bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              M
           </button>
-          <Link
-            to="/activity-info"
-            className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow"
-          >
-            Activity
-          </Link>
-        </div>
-        <div className="relative flex items-center justify-center h-16">
-          <button className="pacifico-title bg-yellow-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            B
-          </button>
-          <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">
-            Activity
+            <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">
+              Activity
           </p>
-        </div>
-        <div className="relative flex items-center justify-center h-16">
-          <button className="pacifico-title bg-red-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            S
+          </div>
+        </Link>
+        <Link to="/activity-info" >
+          <div className="relative flex items-center justify-center h-16">
+            <button className="pacifico-title bg-yellow-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              B
           </button>
-          <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">
-            Activity
+            <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">
+              Activity
           </p>
-        </div>
-      </div>
+          </div>
+        </Link>
+        <Link to="/activity-info" >
+          <div className="relative flex items-center justify-center h-16">
+            <button className="pacifico-title bg-red-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              S
+          </button>
+            <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">
+              Activity
+          </p>
+          </div>
+        </Link>
+      </section>
     </>
   );
 }
