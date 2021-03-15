b836312275097f4b3d1daec95072272d7f1f9f74
James Gould <nobleburgundy@gmail.com>
2021-03-15 12:02:05 -0500

    Passed in prop to CircleButton to render correct letter

--- client/src/components/CircleButton/index.js
+++ client/src/components/CircleButton/index.js
@@ -1,13 +1,12 @@
-/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
 import React from "react";
 
-function CircleButton() {
-    return (
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Button
-        </button>
-    )
+function CircleButton(props) {
+  return (
+    <button className="pacifico-title bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      {props.activityType.substring(0, 1).toUpperCase()}
+    </button>
+  );
 }
-
 
 export default CircleButton;
