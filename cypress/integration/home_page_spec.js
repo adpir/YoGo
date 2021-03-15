8d60885035e6493dbce14ab24c8b7281f9b3cde1
James Gould <nobleburgundy@gmail.com>
2021-03-15 09:21:06 -0500

    Added some smoke tests for all paths through the application

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,8 +1,8 @@
-before(() => {
+beforeEach(() => {
   cy.visit("/");
 });
 
-describe("Home page", () => {
+describe("Smoke Tests", () => {
   it("YGOO Header Loads", () => {
     cy.get("h1").should("be.visible").and("contain", "YOGO");
   });
@@ -14,32 +14,48 @@ describe("Home page", () => {
       .should("be.visible")
       .get("input[data-test=login-remember]")
       .should("be.visible")
-      // .get("button[data-test=login-submit]")
-      // .should("be.visible")
-      // .get("button[data-test=create-account]")
-      // .should("be.visible")           
-      // .get("button[data-test=skip-login]")
-      // .should("be.visible")           
+      .get("button[data-test=sign-in-button]")
+      .should("be.visible");
   });
-  
-  // it("Create account form loads correctly", () => {
-  //   cy.get("button[data-test=login]")
-  //     .should("be.visible")
-  //     // .get("button[data-test=create-account]")
-  //     // .should("be.visible")
-  //     .get("button[data-test=user-name]")
-  //     .should("be.visible")    
-  //     .get("button[data-test=first-name]")
-  //     .should("be.visible")    
-  //     .get("button[data-test=last-name]")
-  //     .should("be.visible")    
-  //     .get("button[data-test=email]")
-  //     .should("be.visible")    
-  //     .get("button[data-test=password]")
-  //     .should("be.visible")    
-  //     .get("button[data-test=create-account-button]")
-  //     .should("be.visible")    
-  //     .get("button[data-test=skip-login]")
-  //     .should("be.visible");      
-  // });
+
+  it("Create Activity Form Loads", () => {
+    cy.get("[data-test=skip-login]").click();
+    cy.get("[data-test=create-activity]").click();
+    cy.contains("ACTIVITY NAME").should("be.visible");
+    cy.contains("Duration").should("be.visible");
+    cy.contains("Frequency").should("be.visible");
+    cy.get("textarea").should("be.visible");
+    cy.contains("CREATE!").should("be.visible");
+  });
+
+  it("Select System Activity page loads with Mind, Body, Social, All buttons", () => {
+    cy.get("[data-test=skip-login]").click();
+    cy.get("[data-test=select-activity]").click();
+    cy.get("[data-test=select-body-activities]")
+      .should("be.visible")
+      .get("[data-test=select-social-activities]")
+      .should("be.visible")
+      .get("[data-test=select-all-activities]")
+      .should("be.visible");
+  });
+
+  // Mind, Body, Social, All paths
+  it("Select System Activity Mind Activity Info page loads", () => {
+    cy.get("[data-test=skip-login]").click();
+    cy.get("[data-test=select-activity]").click();
+    cy.get(`[data-test=select-mind-activities]`).click();
+    cy.contains("TODAY IS THE DAY!").should("be.visible");
+    cy.url().should("include", `day-schedule/mind`);
+  });
+
+  const activityTypes = ["mind", "body", "social", "all"];
+  activityTypes.forEach((activity) => {
+    it(`Select System Activity ${activity} Activity Info page loads`, () => {
+      cy.get("[data-test=skip-login]").click();
+      cy.get("[data-test=select-activity]").click();
+      cy.get(`[data-test=select-${activity}-activities]`).click();
+      cy.contains("TODAY IS THE DAY!").should("be.visible");
+      cy.url().should("include", `day-schedule/${activity}`);
+    });
+  });
 });
