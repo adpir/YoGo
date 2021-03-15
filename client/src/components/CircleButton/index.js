8e5b3a9d432a78a648f8efd9b523f8ffbe32859c
James Gould <nobleburgundy@gmail.com>
2021-03-15 12:37:20 -0500

    Fixed issue with CircleButton props

--- client/src/components/CircleButton/index.js
+++ client/src/components/CircleButton/index.js
@@ -2,9 +2,14 @@
 import React from "react";
 
 function CircleButton(props) {
+  console.log("props", props);
   return (
     <button className="pacifico-title bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-      {props.activityType.substring(0, 1).toUpperCase()}
+      {/* TODO: fix the props passing of info here. Currenly renders 'Y' unless
+      props.activityType is passed a value */}
+      {props.activityType
+        ? props.activityType.substring(0, 1).toUpperCase()
+        : "Y"}
     </button>
   );
 }
