52e5285527d2101519f97317ba6d7284b771d208
James Gould <nobleburgundy@gmail.com>
2021-03-15 12:03:22 -0500

    Added useEffect to get data dynamically for Activity Info page.

--- client/src/pages/activity-info/index.js
+++ client/src/pages/activity-info/index.js
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import CircleButton from "../../components/CircleButton";
 import Navbar from "../../components/Navbar/index";
+import api from "../../utils/api";
 
 export default function ActivityInfo() {
+  const [activity, setActivity] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    api
+      .getSystemActivityById(id)
+      .then((data) => {
+        console.log("activity info data", data);
+        setActivity(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <Navbar />
       <section className="w-full max-w-sm">
-
         <div className="relative flex items-center justify-center h-16">
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold w-screen mx-20 py-2 px-4 border border-gray-400 rounded shadow">
-            ACTIVITY
-        </button>
+          <CircleButton activityType={activity.type} />
+          <p
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold w-screen mx-20 py-2 px-4 border border-gray-400 rounded shadow"
+            data-test="activity-info-name"
+          >
+            {activity.name}
+          </p>
         </div>
-        <div className="relative flex items-center justify-center h-16">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            A
-        </button>
-        </div>
+        <div className="relative flex items-center justify-center h-16"></div>
         <div className="relative flex items-center justify-center  m-2">
-          <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">Duration</p>
-        </div>
-        <div className="relative flex items-center justify-center  m-2">
-          <p className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow">Frequency</p>
+          <label
+            className="relative flex items-center justify-center"
+            htmlFor="duration"
+          >
+            Duration
+          </label>
+          <p
+            className="relative flex  justify-center w-1/2 m-1 font-semibold w-25 py-.5 px-4 border border-gray-400 rounded shadow"
+            data-test="activity-duration-value"
+          >
+            {activity.durationMinutes}m
+          </p>
         </div>
         <div>
-          <p className="border relative rounded flex items-center justify-center m-16 h-40 shadow" rows="4" cols="50">
-            Notes
-        </p>
+          <p
+            className="border relative rounded flex items-center justify-center m-16 h-40 shadow"
+            rows="4"
+            cols="50"
+            id="description"
+            data-test="activity-info-description"
+          >
+            {activity.description}
+          </p>
         </div>
       </section>
     </>
   );
-};
+}
