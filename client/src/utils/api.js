b6f8d782df3ded9c821b991cc331bf3d1befba3d
Phillip Merriman <phillipmerriman@gmail.com>
2021-03-16 13:15:42 -0500

    pulls 2 random activities from each category when user clicks 'all'

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,6 +4,25 @@ const headers = {
   "Content-Type": "text/json",
 };
 
+const shuffle = function (array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 const maxActivitiesReturned = 6;
 
 const api = {
@@ -13,14 +32,16 @@ const api = {
       return responseData;
     });
   },
+
   getSystemActivities: function () {
     return axios
       .get("/api/activities/system")
       .then((res) => {
-        return res.data.slice(0, maxActivitiesReturned);
+        return res.data;
       })
       .catch((err) => console.log(err));
   },
+
   getSystemActivityById: function (id) {
     return axios
       .get("/api/activities/system/" + id)
@@ -29,9 +50,37 @@ const api = {
       })
       .catch((err) => console.log(err));
   },
+
   getSystemActivitiesByType: function (type) {
     // if type is all, get all activities
+    let mind = [];
+    let body = [];
+    let social = [];
+    let twoOfEach = [];
     if (type === "all") {
+      return this.getSystemActivities().then((res) => {
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].type === "mind") {
+            mind.push(res[i]);
+          } else if (res[i].type === "body") {
+            body.push(res[i]);
+          } else {
+            social.push(res[i]);
+          }
+        };
+        
+        twoOfEach.push(mind[0]);
+        twoOfEach.push(body[0]);
+        twoOfEach.push(social[0]);
+        twoOfEach.push(mind[1]);
+        twoOfEach.push(body[1]);
+        twoOfEach.push(social[1]);
+        
+        return twoOfEach;
+      }
+
+      );
+      
       return this.getSystemActivities();
     }
     return axios
