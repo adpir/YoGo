4d9491c70a6e7cceff49296b14ea7c5b72e36f3a
Mae Kindler <mae.akie.kindler@gmail.com>
2021-03-17 06:21:53 -0500

    Activation of navbar burger - styling updates needed, as hamburger button is currently invisible, but the functionality is there.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,20 +2,65 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     // <!-- This example requires Tailwind CSS v2.0+ -->
-    <nav className="bg-yellow-100">
-      <div className="max-w-7xl mx-auto px-2 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center ">
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-yellow-100">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <a className="inline-flex py-6 px-3 mr-4 text-gray-600 hover:text-gray-300 text-4xl font-bold tracking-widest pacifico-title">YOGO</a>
+          <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+            </div>
+            <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  href="#"
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li className="nav-item">
+              <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Create Account
+                </a>
+              </li>
+              <li className="nav-item">
+              <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Sign In
+                </a>
+              </li>
+
+
+            </ul>
+          {/* <div className="absolute inset-y-0 left-0 flex items-center "> */}
             {/* <!-- Mobile menu button--> */}
-            <button
+            {/* <button
               type="button"
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
-              <span className="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span> */}
               {/* <!--
             Icon when menu is closed.
 
@@ -23,7 +68,7 @@ export default function Navbar() {
 
             Menu open: "hidden", Menu closed: "block"
           --> */}
-              <svg
+              {/* <svg
                 className="block h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -37,7 +82,7 @@ export default function Navbar() {
                   strokeWidth="2"
                   d="M4 6h16M4 12h16M4 18h16"
                 />
-              </svg>
+              </svg> */}
               {/* <!--
             Icon when menu is open.
 
@@ -45,7 +90,7 @@ export default function Navbar() {
 
             Menu open: "block", Menu closed: "hidden"
           --> */}
-              <svg
+              {/* <svg
                 className="hidden h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -60,21 +105,21 @@ export default function Navbar() {
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
-            </button>
-          </div>
+            </button> */}
+          {/* </div>
           <div className="flex-1 flex items-center justify-center">
-            <div className="flex-shrink-0 flex items-center">
-              <Link to="/">
+            <div className="flex-shrink-0 flex items-center"> */}
+              {/* <Link to="/">
                 <h1 className="inline-flex py-6 px-3 mr-4 text-gray-600 hover:text-gray-300 text-4xl font-bold tracking-widest pacifico-title">
                   YOGO
                 </h1>
-              </Link>
+              </Link> */}
               {/* <img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"/> */}
-            </div>
+            {/* </div>
             <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
+              <div className="flex space-x-4"> */}
                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                <a
+                {/* <a
                   href="#"
                   className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
@@ -84,28 +129,22 @@ export default function Navbar() {
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Team
+                  Create Account
                 </a>
                 <a
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Projects
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Calendar
+                  Sign In
                 </a>
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span className="sr-only">View notifications</span>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"> */}
+            {/* <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span className="sr-only">View notifications</span> */}
               {/* <!-- Heroicon name: outline/bell --> */}
-              <svg
+              {/* <svg
                 className="h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -120,12 +159,12 @@ export default function Navbar() {
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />
               </svg>
-            </button>
+            </button> */}
 
             {/* <!-- Profile dropdown --> */}
-            <div className="ml-3 relative">
-              <div>
-                <button
+            {/* <div className="ml-3 relative">
+              <div> */}
+                {/* <button
                   type="button"
                   className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                   id="user-menu"
@@ -138,8 +177,8 @@ export default function Navbar() {
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     alt=""
                   />
-                </button>
-              </div>
+                </button> */}
+              {/* </div> */}
 
               {/* <!--
             Dropdown menu, show/hide based on menu state.
@@ -156,21 +195,24 @@ export default function Navbar() {
             <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
             <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
           </div> */}
-            </div>
+            {/* </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1">
+      {/* <div className="sm:hidden" id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1"> */}
           {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
           {/* <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
       <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
       <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
       <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a> */}
-        </div>
+        {/* </div>
       </div>
+    </nav> */}
+    </div>
+    </div>
     </nav>
   );
 }
