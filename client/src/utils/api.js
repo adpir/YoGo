f23674846904b19fe6942bd2920553aa66eeb551
phillipmerriman <phillipmerriman@gmail.com>
2021-03-16 19:12:08 -0500

    now renders 3 random activities based on selected category

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -52,11 +52,11 @@ const api = {
   },
 
   getSystemActivitiesByType: function (type) {
-    // if type is all, get all activities
     let mind = [];
     let body = [];
     let social = [];
-    let twoOfEach = [];
+    let oneOfEach = [];
+    // if type is all, get all activities
     if (type === "all") {
       return this.getSystemActivities().then((res) => {
         for (let i = 0; i < res.length; i++) {
@@ -73,18 +73,55 @@ const api = {
         shuffle(body);
         shuffle(social);
 
-        twoOfEach.push(mind[0]);
-        twoOfEach.push(body[0]);
-        twoOfEach.push(social[0]);
-        twoOfEach.push(mind[1]);
-        twoOfEach.push(body[1]);
-        twoOfEach.push(social[1]);
+        oneOfEach.push(mind[0]);
+        oneOfEach.push(body[0]);
+        oneOfEach.push(social[0]);
         
-        return twoOfEach;
+        return oneOfEach;
       }
 
       );
     }
+
+    if (type === "mind") {
+      return this.getSystemActivities().then((res) => {
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].type === "mind") {
+            mind.push(res[i]);
+          }
+        }
+        shuffle(mind);
+        let sliced = mind.slice(0, 3);
+        return sliced;
+      });
+    }
+    
+    if (type === "body") {
+      return this.getSystemActivities().then((res) => {
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].type === "body") {
+            body.push(res[i]);
+          }
+        }
+        shuffle(body);
+        let sliced = body.slice(0, 3);
+        return sliced;
+      });      
+    }
+    
+    if (type === "social") {
+      return this.getSystemActivities().then((res) => {
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].type === "social") {
+            social.push(res[i]);
+          }
+        }
+        shuffle(social);
+        let sliced = social.slice(0, 3);
+        return sliced;
+      });
+    }
+
     return axios
       .get("/api/activities/system/?type=" + type)
       .then((res) => {
