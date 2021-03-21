cca1b45606090f8ea1b18613039ff2d6d57d4765
Mae Kindler <mae.akie.kindler@gmail.com>
2021-03-21 15:27:26 -0500

    Center Create Account page.

--- client/src/pages/create-account/index.js
+++ client/src/pages/create-account/index.js
@@ -27,16 +27,17 @@ export default function CreateAcct() {
   return (
     <>
       <Navbar />
-      <div className="mt-10 sm:mt-0">
-        <div className="md:grid md:grid-cols-3 md:gap-6">
-          <div className="md:col-span-1">
+      <div className="quicksand-body w-full">
+      <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">
+              <h3 className="text-lg font-medium leading-6 text-gray-900 text-center">
                 Create a free account
               </h3>
               <p className="mt-1 text-sm text-gray-600"></p>
             </div>
           </div>
+        <div className="md:grid md:grid-cols-1 md:gap-6">
+          
           <div className="mt-5 md:mt-0 md:col-span-2">
             <form>
               <div className="shadow overflow-hidden sm:rounded-md">
