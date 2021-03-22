9033ebb26eb07a040f6df594ade99bb90d4c6247
Alice Piar <adpir30@gmail.com>
2021-03-22 15:19:50 -0500

    adding background

--- client/src/pages/create-account/index.js
+++ client/src/pages/create-account/index.js
@@ -26,128 +26,128 @@ export default function CreateAcct() {
 
   return (
     <>
-    <div className="h-screen bkrd-2">
-      <Navbar />
-      <div className="quicksand-body w-full">
-        <div className="md:col-span-1">
-          <div className="relative flex items-center justify-center h-16">
-            <h1 className="pacifico-title text-3xl">Create an Account</h1>
+      <div className="h-screen bkrd-2">
+        <Navbar />
+        <div className="quicksand-body w-full">
+          <div className="md:col-span-1">
+            <div className="relative flex items-center justify-center h-16">
+              <h1 className="pacifico-title text-3xl">Create an Account</h1>
+            </div>
           </div>
-        </div>
-        <div className="md:grid md:grid-cols-1 md:gap-6">
+          <div className="md:grid md:grid-cols-1 md:gap-6">
 
-          <div className="mt-5 md:mt-0 md:col-span-2">
-            <form>
-              <div className="shadow overflow-hidden sm:rounded-md">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="userName"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Username
+            <div className="mt-5 md:mt-0 md:col-span-2">
+              <form>
+                <div className="shadow overflow-hidden sm:rounded-md">
+                  <div className="px-4 py-5 bg-white sm:p-6">
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="userName"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Username
                       </label>
-                      <input
-                        type="text"
-                        name="username"
-                        id="username"
-                        value={user.username}
-                        onChange={(e) => handleChange(e)}
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        data-test="user-name"
-                      />
-                    </div>
+                        <input
+                          type="text"
+                          name="username"
+                          id="username"
+                          value={user.username}
+                          onChange={(e) => handleChange(e)}
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          data-test="user-name"
+                        />
+                      </div>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="first_name"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        First name
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="first_name"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          First name
                       </label>
-                      <input
-                        type="text"
-                        name="first_name"
-                        id="first_name"
-                        value={user.first_name}
-                        onChange={(e) => handleChange(e)}
-                        data-test="first_name"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
+                        <input
+                          type="text"
+                          name="first_name"
+                          id="first_name"
+                          value={user.first_name}
+                          onChange={(e) => handleChange(e)}
+                          data-test="first_name"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label
-                        htmlFor="last_name"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Last name
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="last_name"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Last name
                       </label>
-                      <input
-                        type="text"
-                        name="last_name"
-                        id="last_name"
-                        value={user.last_name}
-                        onChange={(e) => handleChange(e)}
-                        data-test="last_name"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
+                        <input
+                          type="text"
+                          name="last_name"
+                          id="last_name"
+                          value={user.last_name}
+                          onChange={(e) => handleChange(e)}
+                          data-test="last_name"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
 
-                    <div className="col-span-6 sm:col-span-4">
-                      <label
-                        htmlFor="email_address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Email address
+                      <div className="col-span-6 sm:col-span-4">
+                        <label
+                          htmlFor="email_address"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Email address
                       </label>
-                      <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        value={user.email}
-                        onChange={(e) => handleChange(e)}
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        data-test="email"
-                      />
-                    </div>
+                        <input
+                          type="text"
+                          name="email"
+                          id="email"
+                          value={user.email}
+                          onChange={(e) => handleChange(e)}
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          data-test="email"
+                        />
+                      </div>
 
-                    <div className="col-span-6 sm:col-span-4">
-                      <label
-                        htmlFor="password"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Password
+                      <div className="col-span-6 sm:col-span-4">
+                        <label
+                          htmlFor="password"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Password
                       </label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        value={user.password}
-                        onChange={(e) => handleChange(e)}
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        data-test="password"
-                      />
+                        <input
+                          type="password"
+                          name="password"
+                          id="password"
+                          value={user.password}
+                          onChange={(e) => handleChange(e)}
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                          data-test="password"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      data-test="create-account-button"
-                      onClick={(e) => handleSubmit(e)}
-                      type="submit"
-                    >
-                      Create Account!
+                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-900 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        data-test="create-account-button"
+                        onClick={(e) => handleSubmit(e)}
+                        type="submit"
+                      >
+                        Create Account!
                     </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
