53aba3ef3e039ae5abf06f9b54d18f72d1956407
James Gould <nobleburgundy@gmail.com>
2021-03-31 18:51:59 -0500

    Updated Duration field on CreateActivity page to type=number

--- client/src/pages/create-activity/index.js
+++ client/src/pages/create-activity/index.js
@@ -33,6 +33,14 @@ export default function CreateActivity(props) {
     }
   }, []);
 
+  const setDurationField = (e) => {
+    const value = e.target.value;
+    // max duration minutes 999
+    if (value.length <= 3) {
+      setDurationMinutes(value);
+    }
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     const newActivity = {
@@ -60,81 +68,79 @@ export default function CreateActivity(props) {
 
   return (
     <>
-    <div className="h-screen bkrd-5">
-      <Navbar />
-      <form onSubmit={handleFormSubmit}>
-        <div className="quicksand-body w-full">
-          <div className="relative flex items-center justify-center w-3/4 h-16">
-            <input
-              type="text"
-              className="bg-white hover:bg-gray-100 text-gray-800  w-screen mx-5 py-2 px-4 border border-gray-400 rounded shadow"
-              placeholder="Activity Name"
-              data-test="activity-name-field"
-              value={activityName}
-              onChange={(e) => setActivityName(e.target.value)}
-            />
+      <div className="h-screen bkrd-5">
+        <Navbar />
+        <form onSubmit={handleFormSubmit}>
+          <div className="quicksand-body w-full">
+            <div className="relative flex items-center justify-center w-3/4 h-16">
+              <input
+                type="text"
+                className="bg-white hover:bg-gray-100 text-gray-800  w-screen mx-5 py-2 px-4 border border-gray-400 rounded shadow"
+                placeholder="Activity Name"
+                data-test="activity-name-field"
+                value={activityName}
+                onChange={(e) => setActivityName(e.target.value)}
+              />
+            </div>
+            <div className="relative flex items-center justify-center h-16">
+              <label className="flex-grow w-full mx-5  py-.5 px-4">
+                Select Category
+              </label>
+              <select
+                className="flex-grow w-full mx-5"
+                data-test="category-dropdown"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option className="pacifico-title bg-blue-700 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
+                  Mind
+                </option>
+                <option className="pacifico-title bg-yellow-400 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
+                  Body
+                </option>
+                <option className="pacifico-title bg-red-300 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
+                  Social
+                </option>
+                <option className="pacifico-title bg-red-300 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
+                  Other
+                </option>
+              </select>
+            </div>
+            <div className="relative flex items-center justify-center  m-2">
+              <input
+                type="number"
+                data-test="duration-field"
+                className="relative flex justify-center m-1  py-.5 px-4 border border-gray-400 rounded shadow"
+                placeholder="Duration (in minutes)"
+                value={durationMinutes}
+                onChange={(e) => setDurationField(e)}
+              />
+              Minutes
+            </div>
+            <div className="relative flex items-center justify-center  m-2">
+              <textarea
+                className="border relative rounded flex-grow items-center justify-center m-5 p-2  h-40 shadow"
+                rows="4"
+                cols="30"
+                placeholder="Description"
+                data-test="description-field"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <InfoModel
+                hidden={!showInfoMessage}
+                message={activityName + " created successfully."}
+              />
+            </div>
+            <div className="relative flex items-center justify-center w-3/4 m-2">
+              <RectangleBtn
+                disabled={createButtonDisabled}
+                type="submit"
+                buttonText={"CREATE!"}
+              />
+            </div>
           </div>
-          <div className="relative flex items-center justify-center h-16">
-            <label className="flex-grow w-full mx-5  py-.5 px-4">
-              Select Category
-            </label>
-            <select
-              className="flex-grow w-full mx-5"
-              data-test="category-dropdown"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option className="pacifico-title bg-blue-700 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
-                Mind
-              </option>
-              <option className="pacifico-title bg-yellow-400 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
-                Body
-              </option>
-              <option className="pacifico-title bg-red-300 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
-                Social
-              </option>
-              <option className="pacifico-title bg-red-300 hover:bg-blue-700 text-white font-bold mx-2 py-2 px-4 rounded-full">
-                Other
-              </option>
-            </select>
-          </div>
-          <div className="relative flex items-center justify-center  m-2">
-            <input
-              type="text"
-              pattern="\d*"
-              maxLength="3"
-              data-test="duration-field"
-              className="relative flex justify-center m-1  py-.5 px-4 border border-gray-400 rounded shadow"
-              placeholder="Duration (in minutes)"
-              value={durationMinutes}
-              onChange={(e) => setDurationMinutes(e.target.value)}
-            />
-            Minutes
-          </div>
-          <div className="relative flex items-center justify-center  m-2">
-            <textarea
-              className="border relative rounded flex-grow items-center justify-center m-5 p-2  h-40 shadow"
-              rows="4"
-              cols="30"
-              placeholder="Description"
-              data-test="description-field"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <InfoModel
-              hidden={!showInfoMessage}
-              message={activityName + " created successfully."}
-            />
-          </div>
-          <div className="relative flex items-center justify-center w-3/4 m-2">
-            <RectangleBtn
-              disabled={createButtonDisabled}
-              type="submit"
-              buttonText={"CREATE!"}
-            />
-          </div>
-        </div>
-      </form>
+        </form>
       </div>
     </>
   );
